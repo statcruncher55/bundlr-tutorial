ea87afb8b2f47df4d9dbabd43d5f983b9485dc37
Statcruncher55 <37373677+statcruncher55@users.noreply.github.com>
2023-12-18 19:24:09 -0600

    uploading image and metadata json now

--- utils/utils.js
+++ utils/utils.js
@@ -1,34 +1,32 @@
 import * as fs from "fs";
-
-
+import Irys from "@irys/sdk";
 
 const { Private_Key } = process.env;
 
 const getirysClient = () => {
-    const irys = new Irys({
-        url: "https://devnet.irys.xyz",
-        token: "matic",
-        key: Private_Key,
-        config: {
-            providerUrl: "https://rpc-mumbai.maticvigil.com",
-        }
-    });
-    // Print your wallet address
-    console.log(`wallet address = ${irys.address}`);
-    return irys;
+  const irys = new Irys({
+    url: "https://devnet.irys.xyz",
+    token: "solana",
+    key: Private_Key,
+    config: {
+      providerUrl: "https://api.devnet.solana.com",
+    },
+  });
+  // Print your wallet address
+  console.log(`wallet address = ${irys.address}`);
+  return irys;
 };
 
 export const lazyFundNode = async (size) => {
-    const irys = getirysClient();
-    const price = await irys.getPrice(size);
-    await irys.fund(price);
+  const irys = getirysClient();
+  const price = await irys.getPrice(size);
+  await irys.fund(price);
 };
 
 export const uploadFileToArweave = async (filepath, tags) => {
-    const irys = getirysClient();
-    const file = fs.readFileSync(filepath);
-    const { id } = await irys.upload(file, { tags });
-    console.log("file uploaded to ", `https://arweave.net/${id}`);
-    return id;
+  const irys = getirysClient();
+  const file = fs.readFileSync(filepath);
+  const { id } = await irys.upload(file, { tags });
+  console.log("file uploaded to ", `https://arweave.net/${id}`);
+  return id;
 };
-
